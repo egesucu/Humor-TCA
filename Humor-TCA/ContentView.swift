3b3e314d8839c766beba5ef2f5eeb6f5fd10de7b
Ege Sucu <iletisim@egesucu.com.tr>
2025-03-28 17:38:41 +0300

    Initial Commit

---
+++ Humor-TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Humor-TCA
+//
+//  Created by Sucu, Ege on 28.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Humor-TCA/Humor_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Humor_TCAApp.swift
+//  Humor-TCA
+//
+//  Created by Sucu, Ege on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Humor_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
